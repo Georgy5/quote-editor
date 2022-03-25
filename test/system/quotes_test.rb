e6db9616c8daddb978e82a412c840489d9a5df5c
Karl Keller <kgk86@hotmail.com>
2022-03-25 11:57:45 +0100

    Generate initial Rails system test with empty test for creating a new quote

---
+++ test/system/quotes_test.rb
@@ -0,0 +1,17 @@
+require "application_system_test_case"
+
+class QuotesTest < ApplicationSystemTestCase
+  test "Creating a new quote" do
+    # When we visit the Quotes#index page
+    # we expect to see a title with the text "Quotes"
+
+    # When we click on the link with the text "New quote"
+    # we expect to land on a page with the title "New quote"
+
+    # When we fill in the name input with "Capybara quote"
+    # and we click on "Create quote",
+
+    # we expect to be back on the page with the title "Quotes"
+    # and to see our "Capybara quote" added to the list
+  end
+end
