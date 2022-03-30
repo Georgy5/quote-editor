4f61494dbaee7c94ea135172238aadbb30478889
Karl Keller <kgk86@hotmail.com>
2022-03-31 00:54:53 +0200

    Update system tests
    Update Capybara system tests to match new desired behaviour.

--- test/system/quotes_test.rb
+++ test/system/quotes_test.rb
@@ -10,10 +10,11 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "New quote"
-    assert_selector "h1", text: "New quote"
-
     fill_in "Name", with: "Capybara quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Create quote"
+
     assert_selector "h1", text: "Quotes"
     assert_text "Capybara quote"
   end
@@ -30,9 +31,9 @@ class QuotesTest < ApplicationSystemTestCase
     assert_selector "h1", text: "Quotes"
 
     click_on "Edit", match: :first
-    assert_selector "h1", text: "Edit quote"
-
     fill_in "Name", with: "Updated quote"
+
+    assert_selector "h1", text: "Quotes"
     click_on "Update quote"
 
     assert_selector "h1", text: "Quotes"
