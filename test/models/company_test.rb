17a75c21e81754c8ebf20c93002f49a5021ee67c
Karl Keller <kgk86@hotmail.com>
2022-07-02 00:07:04 +0200

    Create helper method #current_company

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get company name" do
+    assert_equal "KPMG", users(:manager).company
+  end
 end
