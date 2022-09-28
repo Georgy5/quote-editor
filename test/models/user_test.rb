fc0ff9d49f9c6f28cb5c27566fe57d1e342b2d76
Karl Keller <kgk86@hotmail.com>
2022-09-28 23:23:13 +0200

    Add test case to User tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,8 @@ class UserTest < ActiveSupport::TestCase
   test "get user name from email" do
     assert_equal "Manager", users(:manager).name
   end
+
+  test "name" do
+    assert_equal"Accountant", users(:accountant).name
+  end
 end
