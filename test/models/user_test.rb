0104d72b4aae69c60eabfd4a590c00c5fded0219
Karl Keller <kgk86@hotmail.com>
2022-09-29 21:27:19 +0200

    Add comments to user tests, incl. future TODO

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,13 @@
 require "test_helper"
 
+# TEST TODO - combine both tests as they test the same thing
 class UserTest < ActiveSupport::TestCase
+  # both test getting the name of the current_user
   test "get user name from email" do
     assert_equal "Manager", users(:manager).name
   end
 
   test "name" do
-    assert_equal"Accountant", users(:accountant).name
+    assert_equal "Accountant", users(:accountant).name
   end
 end
