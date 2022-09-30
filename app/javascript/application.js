c50e7d87ce13c4c117448e7b50c511da981307b6
Karl Keller <kgk86@hotmail.com>
2022-10-01 00:33:38 +0200

    Plan to implement flash messages without Turbo first. Set TODO

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// Disable Turbo on the whole application
+// Make flash messages work without Turbo first
+// TODO: Remove the following lines to Enable Turbo...
+// ...once flash messages are working without it.
